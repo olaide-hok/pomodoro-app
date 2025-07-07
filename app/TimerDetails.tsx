fa9cf5f70abdbb03c8910669452415f989dbe642
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-07 19:02:17 +0100

    refactor component.
    pull of state and state function from useTimer hook.
    
    pass down props to TimerTabs and Timer components.
    
    update SettingsModal component.

--- app/TimerDetails.tsx
+++ app/TimerDetails.tsx
@@ -4,26 +4,36 @@ import SettingsBtn from '@/components/SettingsBtn';
 import SettingsModal from '@/components/SettingsModal';
 import Timer from '@/components/Timer';
 import TimerTabs from '@/components/TimerTabs';
+import {useTimer} from '@/hooks/useTimer';
 import {useState} from 'react';
 
 const TimerDetails = () => {
     const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+    const {timerForm, handleTimerFormChange} = useTimer();
 
     return (
         <>
             <div className="timer-details-wrapper">
                 <div className="timer-tabs-title flex">
-                    <h1 className="title ff-kumbh-sans text-blue-100 fw-700">
+                    <h1
+                        className={`title ff-${timerForm.selectedFont} text-blue-100 fw-700`}>
                         pomodoro
                     </h1>
-                    <TimerTabs />
+                    <TimerTabs
+                        font={timerForm.selectedFont}
+                        bgColor={timerForm.selectedColor}
+                    />
                 </div>
-                <Timer />
+                <Timer timerForm={timerForm} />
 
                 <SettingsBtn openSettings={() => setIsSettingsOpen(true)} />
             </div>
             {isSettingsOpen && (
-                <SettingsModal closeModal={() => setIsSettingsOpen(false)} />
+                <SettingsModal
+                    closeModal={() => setIsSettingsOpen(false)}
+                    timerForm={timerForm}
+                    handleTimerFormChange={handleTimerFormChange}
+                />
             )}
         </>
     );
