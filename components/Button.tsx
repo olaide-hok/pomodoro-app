bcc137bc5ed1bf198e1d3211eb78661413ae582f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-07 19:07:26 +0100

    recieve function prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,9 @@
-const Button = ({title}: {title: string}) => {
+const Button = ({title, fn}: {title: string; fn: () => void}) => {
     return (
-        <button className="clock-btn fs-16 text-white fw-700" type="button">
+        <button
+            className="clock-btn fs-16 text-white fw-700"
+            onClick={fn}
+            type="button">
             {title}
         </button>
     );
