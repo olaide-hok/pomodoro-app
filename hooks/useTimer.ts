4a6e414d3eefd2725347364c5d8843f1a7de61b1
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-07 19:06:10 +0100

    feat(hooks/useTimer): Implement useTimer hook for managing timer form state
    * Added [TimerFormState] interface to define the shape of the timer form state
    * Implemented [useTimer] hook to manage timer form state
    * Added [handleTimerFormChange]function to update timer form state
    * Returned an object containing the current timer form state and update functions

---
+++ hooks/useTimer.ts
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+
+export interface TimerFormState {
+    pomodoro: number;
+    shortBreak: number;
+    longBreak: number;
+    selectedFont: string;
+    selectedColor: string;
+}
+
+export function useTimer() {
+    const [selectedFont, setSelectedFont] = useState('kumbh-sans');
+
+    const [timerForm, setTimerForm] = useState<TimerFormState>({
+        pomodoro: 25,
+        shortBreak: 5,
+        longBreak: 15,
+        selectedFont: 'kumbh-sans',
+        selectedColor: 'red-400',
+    });
+
+    const handleTimerFormChange = (
+        name: keyof TimerFormState,
+        value: string | number
+    ): void => {
+        setTimerForm((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    console.log('timer form--', JSON.stringify(timerForm, null, 2));
+
+    return {
+        selectedFont,
+        setSelectedFont,
+        timerForm,
+        setTimerForm,
+        handleTimerFormChange,
+    };
+}
